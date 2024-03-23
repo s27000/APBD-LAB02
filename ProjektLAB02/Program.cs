3ef96c7990c05e9e6ddca0b8c35c1971f37d7c34
PC <s27000@pjwstk.edu.pl>
2024-03-23 18:51:21 +0100

    commit

--- ProjektLAB02/Program.cs
+++ ProjektLAB02/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Container nowyKontener = new Container("KON-C-1");
+            Container nowyKontener = new Container("KON-C-1", 10000, 10000, 100);
         }
     }
 }
